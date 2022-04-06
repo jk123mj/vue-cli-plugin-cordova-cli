eff13f5dee641dfd76f9bb8f693f799a8e70efaa
xll <2640975676@qq.com>
2022-04-06 13:36:50 +0800

    style(config): 调整forward-server

--- forward-server.js
+++ forward-server.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 let cordovaConfig = fs.readFileSync(process.env.CORDOVA_CONFIG_URL, 'utf-8')
 let lines = cordovaConfig.split(/\r?\n/g)
-let contentIndex = lines.findIndex(v => v.test(/\s+<content/))
+let contentIndex = lines.findIndex(v => /\s+<content/.test(v))
 lines[contentIndex] = `    <content src="${process.env.CORDOVA_SERVER_URL}" />`
 lines.splice(contentIndex + 1, 0, `    <allow-navigation href="${process.env.CORDOVA_SERVER_URL}" />`)
 fs.writeFileSync(process.env.C_CONFIG_URL, lines.join('\n'))
